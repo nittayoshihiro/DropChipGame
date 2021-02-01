6841dede82cc44a021690b804d71c36149068040
bboydaisuke <bboydaisuke@gmail.com>
2021-02-01 13:22:17 +0900

    タイトルシーンを作った

---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TitleManager : MonoBehaviour
+{
+    [SerializeField] PlayableDirector m_director = null;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Skip();
+        }
+    }
+
+    void Skip()
+    {
+        Debug.Log("Timeline Stopped.");
+        //playableDirector.playableGraph.GetRootPlayable(0).SetSpeed(speed)
+        m_director.playableGraph.GetRootPlayable(0).SetSpeed(60);
+    }
+}
