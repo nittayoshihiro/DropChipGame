196a9f2d45b9f78f75cdaec9f5b4c6b1b1faf0bb
nitta <yosshi3800@gmail.com>
2021-02-04 16:05:55 +0900

    重心を変更できるスクリプトを作成しました

---
+++ Assets/Scripts/CenterOfMass.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterOfMass : MonoBehaviour
+{
+    [SerializeField] Vector2 m_center = Vector2.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
+        rb2d.centerOfMass = m_center;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
