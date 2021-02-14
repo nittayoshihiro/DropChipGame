ab193b8f3d52802ca271805de1492c428867a83d
nitta <yosshi3800@gmail.com>
2021-02-14 11:30:02 +0900

    名前の変更とセットアップエラーを防ぐためRequireComponentを追加しました

--- Assets/Scripts/CenterOfMass.cs
+++ Assets/Scripts/CenterOfMass.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class CenterOfMass : MonoBehaviour
 {
     [SerializeField] Vector2 m_center = Vector2.zero;
@@ -20,7 +21,7 @@ public class CenterOfMass : MonoBehaviour
 
     /// <summary>重心を変更します</summary>
     /// <param name="vector2">センターから動かしたい値</param>
-    public void CenterGravityPosChange(Vector2 vector2)
+    public void ChangeCenterOfGravity(Vector2 vector2)
     {
         Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
         rb2d.centerOfMass = vector2;
